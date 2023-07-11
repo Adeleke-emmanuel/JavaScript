5f33da02a87facd07f647ecf55a3c4021bc2ae39
Adeleke-emmanuel <135076483+Adeleke-emmanuel@users.noreply.github.com>
2023-07-11 14:33:16 +0100

    first commit

---
+++ practice.js
@@ -0,0 +1,109 @@
+
+// let name = prompt(`Enter your name: `);
+// console.log(`my name is ${name}`);
+// alert(name);
+// console.log(5+5);
+// console.log(5*1);
+// console.log(10/2);
+// console.log(10%3);
+// console.log(2**2);
+
+// let x = parseInt(prompt(`enter value for x: `));
+// let y = parseInt(prompt(`enter value for y: `));
+
+// let add = x + y;
+// console.log(add);
+// let sub = x - y;
+// console.log(sub);
+// let mult = x * y;
+// console.log(mult);
+// let div = x / y;
+// console.log(div);
+// let squ = x**y;
+// console.log(squ);
+
+// comparison operator
+//== equals
+//!= not quals to
+//=== strictly equals too
+//> greater than
+//< less than
+// >= greater than or equals too
+// <= less than or equals too
+//!== strictly not equals too
+
+//logic operator
+// && and 
+// || or
+
+//+=
+
+// function add(a,b){
+//     return a + b;
+// }
+
+// console.log(add(3,1));
+
+// let substract = function(a,b){
+//     return a - b;
+// }
+
+// console.log(substract(3,1));
+
+// let age = 5;
+
+// if(age >= 5){
+//     console.log(`you can go higher`);
+// }else{
+//     console.log(`enter a valid number greater than 5`);
+// }
+
+// function decodeColor(code){
+//     switch(code){
+//         case 1:
+//             console.log(`Red`);
+//             break;
+//         case 2:
+//             console.log(`green`);
+//             break;
+//         case 3:
+//             console.log(`blue`);
+//             break;
+//         case `x`:
+//             console.log(`yellow`);
+//             break;
+//         default:
+//             console.log(`unknown code`);
+//     }
+// }
+
+// decodeColor(`f`);
+
+// let num = [1,2,3,4,5,6];
+
+// let sum = 0;
+// let i = 0;
+
+// while(i < num.length){
+//     sum += num[i]
+//     i += 1;
+// }
+
+// console.log(i);
+// console.log(sum);
+
+// let num = [1,2,3,4,5,6];
+// let sum = 0;
+// let i = 0;
+
+// do{
+//     sum += num[i];
+//     i++;
+// }while(i < num.length);
+// console.log(i);
+// console.log(sum);
+
+for( let i = 1; i <= 100; i += 1 ){
+    console.log(i);
+}
+
